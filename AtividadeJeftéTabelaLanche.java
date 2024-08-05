0fe206c41e9cd8e4414b4b28f0c07000d9348aa0
Luiz Eduardo <146491566+dudunasc@users.noreply.github.com>
2024-08-05 06:25:28 -0300

    Add files via upload

---
+++ AtividadeJeftéTabelaLanche.java
@@ -0,0 +1,20 @@
+package atividadejeftétabelalanche;
+
+import java.util.Scanner;
+
+public class AtividadeJeftéTabelaLanche {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       
+       int[] codigos = {1, 2, 3, 4, 5};
+       String [] nomes = {"Cachorro quente", "X-Salada", "X-Bacon", "Torrada simples", "Refrigerante"};
+       double[] precos = {4.00, 4.50, 5.00, 2.00, 1.50};
+       
+        System.out.println("Qual o código do item desejado?");
+        int codigo = scanner.nextInt();
+        
+       
+    }
+    
+}
